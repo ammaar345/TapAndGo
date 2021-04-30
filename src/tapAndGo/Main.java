affe14658b193a0068b00e4ad212c31cee6755e0
Ammaar <fakirammaar8@gmail.com>
2021-04-30 14:10:14 +0200

    completed exercise

--- src/tapAndGo/Main.java
+++ src/tapAndGo/Main.java
@@ -1,4 +1,11 @@
 package tapAndGo;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        TapAndGo tapAndGo = new TapAndGo(20000,1000);
+
+        tapAndGo.deposit(2000);
+        tapAndGo.pay(3000);
+        System.out.println(tapAndGo.getBalance());
+    }
 }
--- src/tapAndGo/TapAndGo.java
+++ src/tapAndGo/TapAndGo.java
@@ -3,21 +3,38 @@ package tapAndGo;
 public class TapAndGo {
     //static variables are shared by a class , while not static , values are not shared between instances.
     double balance;
-   TapAndGo(){
+    //    double newBalance;
+    double threshold;
+
+    TapAndGo(double balance, double threshold) {
+        this.balance = balance;//setting balance
+        this.threshold = threshold;//setting threshold for tap and go
+    }
 
 
-   }
+    public void pay(double payAmount) throws Exception {
+        try {
+            if (payAmount < threshold) {
+                balance = balance - payAmount; //subtracting payment from your balance
 
-
-    public void pay(double payAmount){
-
+            } else {
+                throw new Exception("bread");
+            }
+        } catch (Exception e) {
+            System.out.println("You are exceeding the tap and go limit for your card.");
+        }
 
     }
-    public void deposit(double depositAmount){
 
+    public void deposit(double depositAmount) {
+        balance += depositAmount;//increase balance when you deposit money
 
     }
-    public double getBalance(){
+
+    public double getBalance() {
+
         return balance;
+        //tell us the balance we have in our account
+
     }
 }
