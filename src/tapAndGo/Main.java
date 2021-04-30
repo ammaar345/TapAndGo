756b64ceead96e77e42410d2067cc1cc48631ac9
Ammaar <fakirammaar8@gmail.com>
2021-04-30 09:19:37 +0200

    created skeleton for all needed functions this far

---
+++ src/tapAndGo/Main.java
@@ -0,0 +1,4 @@
+package tapAndGo;
+
+public class Main {
+}
---
+++ src/tapAndGo/TapAndGo.java
@@ -0,0 +1,23 @@
+package tapAndGo;
+
+public class TapAndGo {
+    //static variables are shared by a class , while not static , values are not shared between instances.
+    double balance;
+   TapAndGo(){
+
+
+   }
+
+
+    public void pay(){
+
+
+    }
+    public void deposit(){
+
+
+    }
+    public double getBalance(){
+        return balance;
+    }
+}
